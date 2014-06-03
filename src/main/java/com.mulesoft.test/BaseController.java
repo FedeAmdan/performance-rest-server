5a66e02df8bc19d85fbcf64192ee8f048988ad26
Federico Amdan <federico.amdan@gmail.com>
2014-06-03 14:41:45 -0300

    Adding configurable delay calls

--- src/main/java/com.mulesoft.test/BaseController.java
+++ src/main/java/com.mulesoft.test/BaseController.java
@@ -1,11 +1,13 @@
 package com.mulesoft.test;
 
+import java.util.Random;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.apache.commons.lang.StringUtils;
 
 @Controller
 @RequestMapping("/")
@@ -26,7 +28,32 @@ public class BaseController {
         String repeated = StringUtils.repeat("f",letters);
         model.addAttribute("message", repeated);
         return "index";
-
     }
 
+    @RequestMapping(value="/strictdelay/{miliseconds}", method = RequestMethod.GET)
+    public String strictdelay(@PathVariable int miliseconds, ModelMap model) throws InterruptedException
+    {
+        Thread.sleep(miliseconds);
+        model.addAttribute("message", "Waited for " + miliseconds + " miliseconds");
+        return "index";
+    }
+
+    @RequestMapping(value="/randomdelay/{maxMiliseconds}", method = RequestMethod.GET)
+    public String randomdelay(@PathVariable int maxMiliseconds, ModelMap model) throws InterruptedException
+    {
+        Random r = new Random();
+        int wait = r.nextInt(maxMiliseconds);
+        Thread.sleep(wait);
+        model.addAttribute("message", "Waited for " + wait + " miliseconds");
+        return "index";
+    }
+    //
+    //
+    //@RequestMapping(value = "/add", method = RequestMethod.POST)
+    //public String addUser(@PathVariable("user") String user, ModelMap model) throws InterruptedException {
+    //    int delay = 4000;
+    //    Thread.sleep(delay);
+    //    model.addAttribute("message", "Waited for " + delay + " miliseconds");
+    //    return "index";
+    //}
 }
